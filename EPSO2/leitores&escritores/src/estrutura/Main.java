35caa86d6a66472621318cc9947818a6d5209454
Thiago Padrão <thiagotpadrao@gmail.com>
2024-10-31 17:41:43 -0300

    prioridade trocada (leitores agora tem ela)

--- EPSO2/leitores&escritores/src/estrutura/Main.java
+++ EPSO2/leitores&escritores/src/estrutura/Main.java
@@ -2,31 +2,33 @@ package estrutura;
 
 import estrutura.threads.Reader;
 import estrutura.threads.Writer;
+import estrutura.threads.SharedControl;
 import estrutura.dados.Data;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
 
 public class Main {
+    
+    public static final int numReaders = 5;
+    public static final int numWriters = 5;
+    
     public static void main(String[] args) {
-        // Inicializa a estrutura de dados e carrega o arquivo
         Data data = new Data("bd.txt");
-        ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
-        
-        // Popula o array de threads de maneira aleatória com Readers e Writers
-        List<Thread> threads = new ArrayList<>();
-        populateThreadsArray(threads, data, lock, 10, 10);
+        ReentrantLock lock = new ReentrantLock();
+        Condition canWrite = lock.newCondition();
+        SharedControl sharedControl = new SharedControl();
 
-        // Marca o início da contagem do tempo
+        List<Thread> threads = new ArrayList<>();
+        populateThreadsArray(threads, data, lock, canWrite, sharedControl, numReaders, numWriters);
+
         long startTime = System.currentTimeMillis();
-        
-        // Executa cada thread no array
         for (Thread thread : threads) {
             thread.start();
         }
 
-        // Espera todas as threads terminarem
         for (Thread thread : threads) {
             try {
                 thread.join();
@@ -34,27 +36,24 @@ public class Main {
                 e.printStackTrace();
             }
         }
-        
-        // Marca o fim da contagem do tempo
+
         long endTime = System.currentTimeMillis();
         System.out.println("Tempo total de execução: " + (endTime - startTime) + " ms");
 
-        //printa o array
         data.logData("logFile.txt");
     }
 
-    private static void populateThreadsArray(List<Thread> threads, Data data, ReentrantReadWriteLock lock, int numReaders, int numWriters) {
+    private static void populateThreadsArray(List<Thread> threads, Data data, ReentrantLock lock, Condition canWrite, SharedControl sharedControl, int numReaders, int numWriters) {
         List<Thread> threadList = new ArrayList<>();
 
         for (int i = 0; i < numReaders; i++) {
-            threadList.add(new Thread(new Reader(data, lock)));
+            threadList.add(new Thread(new Reader(data, lock, canWrite, sharedControl)));
         }
 
         for (int i = 0; i < numWriters; i++) {
-            threadList.add(new Thread(new Writer(data, lock)));
+            threadList.add(new Thread(new Writer(data, lock, canWrite, sharedControl)));
         }
 
-        // Embaralha a lista para garantir a aleatoriedade
         Collections.shuffle(threadList);
         threads.addAll(threadList);
     }
